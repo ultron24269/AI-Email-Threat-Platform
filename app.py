31a762296d8297680c9a1d534e304ed015cb7a27
ultron24269 <vijiviji24269@gmail.com>
2026-09-10 20:26:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Inserts the absolute root directory path into Python's search path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import streamlit as st
 import imaplib
 import email
